10fb76ef178683e15306406dec5bfa2e3336199b
Asjad Usmani <asjadusmani4@gmail.com>
2026-03-09 15:51:20 +0530

    Duplicate backend files deleted

--- app/api/workspace/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { menusRoutes } from './menus/menus.route';
-
-const router = Router();
-
-router.use('/menus', menusRoutes);
-
-export { router as workspaceRoutes };
-
